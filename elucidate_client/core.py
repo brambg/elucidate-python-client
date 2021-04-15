fa4746844c6169a04eab095f38b08cea080a7942
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2021-04-15 17:13:58 +0200

    add ElucidateClient + test

--- elucidate_client/core.py
+++ elucidate_client/core.py
@@ -1,3 +1,62 @@
+import requests
+
+
 def hello():
     '''hello...'''
     print('hello')
+
+
+headers = {
+    'Accept': 'application/ld+json; profile="http://www.w3.org/ns/anno.jsonld"',
+    'Content-Type': 'application/ld+json; profile="http://www.w3.org/ns/anno.jsonld"'
+}
+
+
+class ContainerIdentifier():
+    def __init__(self, url: str):
+        self.url = url
+        self.uuid = url.split('/')[-2]
+
+
+class AnnotationIdentifier():
+    def __init__(self, url: str):
+        self.url = url
+        url_split = url.split('/')
+        self.uuid = url_split[-2]
+        self.container_uuid = url_split[-3]
+
+
+class ElucidateClient():
+    def __init__(self, base_uri: str):
+        self.base_uri = base_uri
+        self.version = 'w3c'
+
+    def use_w3c(self):
+        self.version = 'w3c'
+
+    def use_oa(self):
+        self.version = 'oa'
+
+    def create_container(self, label: str = 'A Container for Web Annotations'):
+        body = {
+            "@context": [
+                "http://www.w3.org/ns/anno.jsonld",
+                "http://www.w3.org/ns/ldp.jsonld"
+            ],
+            "type": [
+                "BasicContainer",
+                "AnnotationCollection"
+            ],
+            "label": label
+        }
+        response = requests.post(url=f'{self.base_uri}/w3c/',
+                                 headers=headers,
+                                 json=body)
+        container_id = response.headers['location']
+        return ContainerIdentifier(container_id)
+
+    def get_container(self, container_identifier: ContainerIdentifier):
+        return requests.get(f'{self.base_uri}/{self.version}/{container_identifier.uuid}/').json()
+
+    def get_annotation(self, annotation_identifier: AnnotationIdentifier):
+        return requests.get(f'{self.base_uri}/{self.version}/{annotation_identifier.container_uuid}/{annotation_identifier.uuid}/').json()
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+from icecream import ic
 
+from core import ElucidateClient
 from .context import elucidate_client
 
 import unittest
+import icecream
 
 
 class BasicTestSuite(unittest.TestCase):
@@ -11,11 +14,37 @@ class BasicTestSuite(unittest.TestCase):
     def test_absolute_truth_and_meaning(self):
         assert True
 
+
 class ProjectTestSuite(unittest.TestCase):
     """project test cases."""
 
     def test_hello(self):
         elucidate_client.hello()
+
+
+class ElucidateClientTestSuite(unittest.TestCase):
+    """Elucidate Client test cases."""
+
+
+    def test_container(self):
+        ec = ElucidateClient("http://localhost:8080/annotation")
+        container_id = ec.create_container(label='Annotation Container')
+        assert container_id != None
+        ic(container_id.url)
+        ic(container_id.uuid)
+
+        w3c_container = ec.get_container(container_id)
+        assert w3c_container != None
+        ic(w3c_container)
+        ic(w3c_container['id'])
+        assert '/w3c/' in w3c_container['id']
+
+        ec.use_oa()
+        oa_container = ec.get_container(container_id)
+        assert oa_container != None
+        ic(oa_container)
+        ic(oa_container['id'])
+        assert '/oa/' in oa_container['id']
 
 
 if __name__ == '__main__':
